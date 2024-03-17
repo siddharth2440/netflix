c9455891a06617201ef65dbd7bfef283a75cb487
siddharth2440 <siddharth2440.live@gmail.com>
2024-03-17 11:01:20 +0530

    movieModel created

---
+++ models/movie.model.js
@@ -0,0 +1,54 @@
+import {Schema, model} from "mongoose"
+
+const movieSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+        minLength:[5,"Movie Title must be atleast 5 characters"]
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true,
+        minLength:[10,"Movie Description must be atleast 10 characters"]
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    imageSm:{
+        type:String,
+        required:true
+    },
+    imageTitle:{
+        type:String,
+        required:true
+    },
+    trailer:{
+        type:String,
+        required:true
+    },
+    video:{
+        type:String,
+        required:true
+    },
+    year:{
+        type:String
+    },
+    lmit:{
+        type:Number
+    },
+    genre:{
+        type:String,
+    },
+    isSeries:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+})
+
+const movieModel = new model("Movie",movieSchema)
+
+export default movieModel;
